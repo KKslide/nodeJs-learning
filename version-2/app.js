83cd9e9975698aa354af14f1fbbb23158f31b020
liuyongkang <liuyongkang@appgame.com>
2018-12-04 10:40:57 +0800

    判断data文件夹是否存在

--- version-2/app.js
+++ version-2/app.js
@@ -1,7 +1,25 @@
 var http = require("http");
+var fs = require("fs");
 var router = require("./lib/router.js");
 
 var server = http.createServer();
+
+// 判断有没有data文件夹存放数据
+fs.exists("./data", function (exists) {
+    if (!exists) { // 文件夹不存在
+        fs.mkdir("./data", (err) => {
+            if (err) {
+                console.log(err);
+                return false;
+            } else {
+                fs.appendFile("./data/data.json", '{"heros":[]}', (err) => {
+                    if (err) return console.log(err);
+                })
+            }
+        })
+    }
+});
+
 server.listen(3001, () => {
     console.log('http:127.0.0.1:3001');
 });
